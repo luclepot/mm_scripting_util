bde3172fd6dd7ae912637f902c1e40d5e76d767b
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-02-26 14:11:19 +0100

    added simulation tests

---
+++ tests/test_simulate_data.py
@@ -0,0 +1,26 @@
+import mm_scripting_util as mm
+
+def test_simulate_basic(): 
+    t = mm.core.miner(
+        name="simulate_basic",
+        loglevel=10,
+        autodestruct=True,
+        backend="tth.dat"
+    )
+    t.simulate_data(
+        samples=10000,
+        sample_benchmark='w'
+    )
+
+def test_simulate_step():
+    t = mm.core.miner(
+        name="simulate_basic",
+        loglevel=10,
+        autodestruct=True,
+        backend="tth.dat"
+    )
+    t.setup_cards(10000)
+    t.simulate_data(
+        samples=10000,
+        sample_benchmark='sm'
+    )
