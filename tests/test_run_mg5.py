c6490cfd4bb20206a69c16731d8f8649882a6d4d
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-02-26 12:15:53 +0100

    new test run mg5 stuff

--- tests/test_run_mg5.py
+++ tests/test_run_mg5.py
@@ -4,7 +4,34 @@ def test_run_basic():
     t = mm.core.miner(
         name="mg5_run_small",
         loglevel=10,
-        autodestruct=False
+        autodestruct=True,
+        backend="tth.dat"
+    )
+    t.setup_cards(
+        n_samples=1000,
+        force=True
+    )
+    t.run_morphing()
+    t.setup_mg5_scripts(
+        sample_benchmark='sm',
+        samples=1000,
+        force=True
+    )
+    t.run_mg5_script(
+        samples=1000,
+        platform="lxplus7",
+        force=True
+    )
+    assert(t._check_valid_mg5_run(
+        samples=1000
+    ))
+
+def test_run_test_backend(): 
+    t = mm.core.miner(
+        name="mg5_run_test_backend",
+        loglevel=10,
+        autodestruct=True,
+        backend="test.dat"
     )
     t.setup_cards(
         n_samples=1000,
