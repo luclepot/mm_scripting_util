128e260dbd1f71770d717625ccad9e06d554bc9c
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-03-08 14:48:20 +0100

    changing core sub/sh fiels

--- mm_scripting_util/data/condor/core.sh
+++ mm_scripting_util/data/condor/core.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# CONDA_PATH = "/afs/cern.ch/work/l/llepotti/anaconda3"
-
-# # __conda_setup="$('$CONDA_PATH/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-
-# # eval "$__conda_setup"
-# # else 
-# #     if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
-# #         . "$CONDA_PATH/etc/profile.d/conda.sh"
-# #     else
-# #         export PATH="$CONDA_PATH/etc/profild.d/conda.sh"
-# #     fi
-# # fi
-
-# # unset __conda_setup
+cd $1
+python -m mm_scripting_util eval $2
