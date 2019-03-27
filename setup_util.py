878ca0936c1bda6cebb98dddd73c7da0db9ab618
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-03-27 09:19:38 +0100

    added setuputil function

--- setup_util.py
+++ setup_util.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import subprocess 
 
 def _conda_info(
 ):
@@ -6,7 +8,14 @@ def _conda_info(
     Returns the current os' installed conda environments, as well as the current env
     Returns empty list, None if conda is not installed.
     """
-    conda_ret = [line.strip("\n").split() for line in os.popen("conda env list").readlines() if line[0] is not "#" and line is not "\n"]
+    python_version = sys.version_info[0:2]
+
+    SPobj = subprocess.Popen(["conda env list"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    conda_ret_raw = SPobj.stdout.readlines() + SPobj.stderr.readlines()
+
+
+    
+    conda_ret = [line.strip("\n").split() for line in conda_ret_raw if line[0] is not "#" and line is not "\n"]
     # handle uninstalled conda case
     if "bash: conda: command not found" in conda_ret:
         return [], None
