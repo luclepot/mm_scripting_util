c90cad3775eebc74ec05f9a50bb251081d870700
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-03-27 09:48:21 +0100

    fixed conda info function for any linux / python version

--- setup_util.py
+++ setup_util.py
@@ -10,17 +10,19 @@ def _conda_info(
     """
     python_version = sys.version_info[0:2]
 
-    SPobj = subprocess.Popen(["conda env list"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    conda_ret_raw = SPobj.stdout.readlines() + SPobj.stderr.readlines()
+    subprocess_object = subprocess.Popen(["conda env list"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    conda_ret_raw = [str(line.decode('utf8')) for line in (subprocess_object.stdout.readlines() + subprocess_object.stderr.readlines())]
 
 
+    conda_ret = [line.strip("\n") for line in conda_ret_raw if line[0] != '#' and line != '\n']
     
-    conda_ret = [line.strip("\n").split() for line in conda_ret_raw if line[0] is not "#" and line is not "\n"]
-    # handle uninstalled conda case
-    if "bash: conda: command not found" in conda_ret:
-        return [], None
+    # handle uninstalled conda cases
+    for line in conda_ret:
+        if "conda: command not found" in line:
+            return [], None
+
     # grab all/current envs
+    conda_ret = [line.split() for line in conda_ret]
     conda_envs = [line[0] for line in conda_ret]
     conda_env_current = conda_ret["*" in conda_ret][0]
     return conda_envs, conda_env_current
-
