e741e55d5b74d07efc7e08e11da3056dfa5909a4
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-04-01 13:47:20 +0200

    removing help message

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,6 @@
 python mm_scripting_util/setup_env_util.py "$@"
 source ~/miniconda/etc/profile.d/conda.sh
 if [ -e setup_env_util.sh ]; then 
-    echo "running setup script..."
     chmod +x setup_env_util.sh;
     source setup_env_util.sh;
     rm setup_env_util.sh;
