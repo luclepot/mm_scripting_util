3d0d3f899af68cb422dd33c87b6a2a226af0267d
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-04-01 13:04:28 +0200

    fixed bug w conda install paths, should work now

--- mm_scripting_util/setup_env_util.py
+++ mm_scripting_util/setup_env_util.py
@@ -93,6 +93,11 @@ def write_environment_setup_script(
 
     conda_installation_directory = "~"
 
+    if conda_installation_directory is not "~":
+        conda_installation_directory = "\"" + conda_installation_directory
+    else:
+        conda_installation_directory = conda_installation_directory + "\""
+
     # conda_envs, current_env = _conda_info()
     
     # open bash file
@@ -102,11 +107,11 @@ def write_environment_setup_script(
 
         if conda_install or run_all:
             f.write("echo 'attempting to install anaconda..'")
-            f.write("wget -nv {0} -O {1}/miniconda.sh".format(anaconda_link, conda_installation_directory))
+            f.write("wget -nv {0} -O {1}/miniconda.sh\"".format(anaconda_link, conda_installation_directory))
             f.write("echo 'got miniconda source?'")
-            f.write("source \"{0}/miniconda.sh\" -b -p \"{1}/miniconda\"".format(conda_installation_directory, conda_installation_directory))
-            f.write("source \"{0}/miniconda/etc/profile.d/conda.sh\"".format(conda_installation_directory))
-            f.write("rm \"{0}/miniconda.sh\"".format(conda_installation_directory))
+            f.write("source {0}/miniconda.sh\" -b -p {1}/miniconda\"".format(conda_installation_directory, conda_installation_directory))
+            f.write("source {0}/miniconda/etc/profile.d/conda.sh\"".format(conda_installation_directory))
+            f.write("rm {0}/miniconda.sh\"".format(conda_installation_directory))
 
         if conda_env_install or run_all:
             f.write("echo 'attempting to create anaconda environment from file'")
