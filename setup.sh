a2792d6f1f1fbfd94c7efc01ac763d6c4cb8ba22
luc le pottier <luc.lepottier@cern.ch>
2019-05-12 17:38:19 +0000

    added small change to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if ! [ -x "$(command -v python)" ]; then
     echo 'setup.sh: no python on path; attempting install...'
     apt-get install python-pip
 fi
-python mm_scripting_util/setup_env_util.py "$@"
+python3 mm_scripting_util/setup_env_util.py "$@" || python mm_scripting_util/setup_env_util.py "$@"
 if [ -e setup_env_util.sh ]; then 
     echo 'starting bash setup script run...'
     chmod +x setup_env_util.sh;
